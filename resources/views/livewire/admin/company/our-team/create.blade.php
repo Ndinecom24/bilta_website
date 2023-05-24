74c8ddcb3b9ae06bb8281d25870a07e81c3583c2
Nimbus <NShubart@zesco.co.zm>
2023-05-24 21:09:30 +0200

    about us link on nav bar

--- resources/views/livewire/admin/company/our-team/create.blade.php
+++ resources/views/livewire/admin/company/our-team/create.blade.php
@@ -12,6 +12,16 @@
             <form enctype="multipart/form-data">
                 <div class="modal-body">
 
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
                     <div class="row">
                         <div class="col-lg-12 col-md-12 col-sm-12">
                             <div class="form-group">
